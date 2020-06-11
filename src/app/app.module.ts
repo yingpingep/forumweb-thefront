bf467fe4f732222c9f4802cbf20b4d8593e6d4f7
yinnping <ciaoing@livemail.tw>
2020-06-12 01:24:23 +0800

    [WIP] Update nickname component
    1. Use css rather than write in html file.
    2. Add a simple form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NicknameComponent } from './nickname/nickname.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { NicknameComponent } from './nickname/nickname.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/nickname/nickname.component.ts
+++ src/app/nickname/nickname.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   templateUrl: './nickname.component.html',
   styleUrls: ['./nickname.component.scss']
 })
 export class NicknameComponent implements OnInit {
-
-  constructor() { }
+  nickName = new FormControl('', Validators.required);
+  nickForm = this.fb.group({
+    nickName: this.nickName
+  });
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    console.log(`🌻: NicknameComponent -> onSubmit -> this.nickForm.value`, this.nickForm.get('nickName'));
+  }
 }
