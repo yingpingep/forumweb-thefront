19e43bab23b00effa006116d81a2365b9e1aeb18
yinnping <ciaoing@livemail.tw>
2020-06-19 01:57:37 +0800

    Update question-card comopnent

--- src/app/dashboard/questions/questions.component.ts
+++ src/app/dashboard/questions/questions.component.ts
@@ -11,15 +11,15 @@ export class QuestionsComponent implements OnInit {
   constructor() { }
 
   movies = [
-    'Episode I - The Phantom Menace',
-    'Episode II - Attack of the Clones',
-    'Episode III - Revenge of the Sith',
-    'Episode IV - A New Hope',
-    'Episode V - The Empire Strikes Back',
-    'Episode VI - Return of the Jedi',
-    'Episode VII - The Force Awakens',
-    'Episode VIII - The Last Jedi',
-    'Episode IX – The Rise of Skywalker'
+    'I',
+    'II',
+    'III',
+    'IV',
+    'V',
+    'VI',
+    'VII',
+    'VIII',
+    'IX'
   ];
 
   ngOnInit(): void {
