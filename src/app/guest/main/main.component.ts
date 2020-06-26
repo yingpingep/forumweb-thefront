33ec43fbc2db6767882e5d2472d49be3075df40c
yinnping <ciaoing@livemail.tw>
2020-06-26 23:57:43 +0800

    Update main component Add submit button.

--- src/app/guest/main/main.component.ts
+++ src/app/guest/main/main.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import {
+  Component,
+  OnInit,
+  ViewChildren,
+  ElementRef,
+  AfterViewInit,
+  QueryList,
+  Renderer2,
+} from '@angular/core';
+import { FormBuilder, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, AfterViewInit {
+  @ViewChildren('optionRefs') optionRefs: QueryList<ElementRef>;
   numberOfQuestion = 'Q1';
   question: Question = {
     id: '1',
     title: '測試用的題目 1234567',
-    mode: 'answer',
+    mode: 'multiple',
     answerOptions: [
       {
         index: 0,
@@ -38,14 +47,67 @@ export class MainComponent implements OnInit {
     answer: '簡答題',
   };
 
+  answers: AnswerOption[] = [];
+
   get optionType() {
     return this.question.mode === 'single' ? 'radio' : 'checkbox';
   }
-  constructor(private fb: FormBuilder) {}
+  constructor(private renderer: Renderer2) {}
 
   ngOnInit(): void {}
 
+  ngAfterViewInit() {}
+
   rollToFirst(event: Event) {
     window.scrollTo(0, 0);
+  }
+
+  answerTextareaChange(event: Event) {
+    const target = event.target as HTMLTextAreaElement;
+    this.answers = [
+      {
+        index: 0,
+        text: target.value,
+      },
+    ];
+  }
+
+  optionChange(option: AnswerOption) {
+    const multiple = () => {
+      const hasChecked =
+        this.answers.filter((v) => v.index === option.index).length === 1;
+      const nativeEle = this.optionRefs.find(
+        (_, index) => index === option.index
+      ).nativeElement;
+      if (hasChecked) {
+        this.renderer.removeClass(nativeEle, 'active');
+        this.answers = this.answers.filter((v) => v.index !== option.index);
+        return;
+      }
+
+      this.renderer.addClass(nativeEle, 'active');
+      this.answers.push(option);
+    };
+
+    const single = () => {
+      this.optionRefs.forEach((item, index) => {
+        if (index !== option.index) {
+          this.renderer.removeClass(item.nativeElement, 'active');
+        } else {
+          this.renderer.addClass(item.nativeElement, 'active');
+        }
+      });
+      this.answers = [option];
+    };
+
+    if (this.question.mode === 'multiple') {
+      multiple();
+    } else {
+      single();
+    }
+  }
+
+  submit() {
+    console.log(...this.answers);
   }
 }
