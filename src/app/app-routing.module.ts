d10e7f5bbb9617595bb48baec1f9311bfb163384
yinnping <ciaoing@livemail.tw>
2020-07-06 22:12:22 +0800

    Rename dashboard module to manager module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
   {
     path: 'dashboard',
     loadChildren: () =>
-      import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+      import('./manager/manager.module').then((m) => m.ManagerModule),
   },
   {
     path: 'guest',
