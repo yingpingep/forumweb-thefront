7916ba55be01a342aaca96b05b316199baad984c
yinnping <ciaoing@livemail.tw>
2020-07-22 23:19:48 +0800

    Add directive test backbone

--- src/app/guest/directives/other-answer.directive.spec.ts
+++ src/app/guest/directives/other-answer.directive.spec.ts
@@ -1,8 +1,40 @@
 import { OtherAnswerDirective } from './other-answer.directive';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { Component, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  selector: 'mock-component',
+  template: ` <button appOtherAnswer>aa</button> `,
+})
+class MockComponent {}
 
 describe('OtherAnswerDirective', () => {
+  let component: MockComponent;
+  let fixture: ComponentFixture<MockComponent>;
+  let mockHostDeEle: DebugElement;
+  let mockHostEle: HTMLElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [MockComponent, OtherAnswerDirective],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(MockComponent);
+    component = fixture.componentInstance;
+    mockHostDeEle = fixture.debugElement.query(
+      By.css('button[appOtherAnswer]')
+    );
+    mockHostEle = mockHostDeEle.nativeElement;
+  }));
+
   it('should create an instance', () => {
     const directive = new OtherAnswerDirective();
     expect(directive).toBeTruthy();
+  });
+
+  it('should create a mock component with directive', () => {
+    expect(component).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('[appOtherAnswer]'))).toBeTruthy();
   });
 });
