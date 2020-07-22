298969c75332dc2605bf19599bdcc789cee4a891
yinnping <ciaoing@livemail.tw>
2020-07-22 21:11:14 +0800

    Add other-answer attribute directive

---
+++ src/app/guest/directives/other-answer.directive.spec.ts
@@ -0,0 +1,8 @@
+import { OtherAnswerDirective } from './other-answer.directive';
+
+describe('OtherAnswerDirective', () => {
+  it('should create an instance', () => {
+    const directive = new OtherAnswerDirective();
+    expect(directive).toBeTruthy();
+  });
+});
